7dc22ad10ce9624805cd379da84a336bfdd32e1d
Michail Mangasarov <mangasarov@hotmail.de>
2025-07-20 20:43:54 +0200

    ja

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import './App.css';
-import Title from './components/Title/Title';
-import Address from './components/Address/Address';
-import Experience from './components/Experience/Experience';
-import Education from './components/Education/Education';
-import Hobbies from './components/Hobbies/Hobbies';
-import Knowledge from './components/Knowledge/Knowledge';
-import Languages from './components/Languages/Languages';
-import Socials from './components/Socials/Socials';
-import Volunteer from './components/Volunteer/Volunteer';
-import Picture from './components/Picture/Picture';
-
+import React from "react";
+import "./App.css";
+import Title from "./components/Title/Title";
+import Address from "./components/Address/Address";
+import Experience from "./components/Experience/Experience";
+import Education from "./components/Education/Education";
+import Hobbies from "./components/Hobbies/Hobbies";
+import Knowledge from "./components/Knowledge/Knowledge";
+import Languages from "./components/Languages/Languages";
+import Socials from "./components/Socials/Socials";
+import Volunteer from "./components/Volunteer/Volunteer";
+import Picture from "./components/Picture/Picture";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="top">
-          <Title/>
-          <Picture/>
+          <Title />
+          <Picture />
         </div>
         <div className="socialsAddress">
-          <Socials/>
-          <Address/>
+          <Socials />
+          <Address />
         </div>
         <div className="core">
-        <Experience/>
-        <Knowledge/>
-        <Languages/>
-        <Education/>
-        <Volunteer/>
-        <Hobbies/>
+          <Experience />
+          <Knowledge />
+          <Languages />
+          <Education />
+          <Volunteer />
+          <Hobbies />
         </div>
       </header>
     </div>
